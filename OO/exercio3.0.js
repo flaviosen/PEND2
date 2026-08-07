4479a6e04e5027b4e7d0c1e8969ad4933162ce74
flaviosen <flavio.izidoro@aluno.senai.br>
2026-08-07 11:22:08 -0300

    exercio3.0.html

---
+++ OO/exercio3.0.js
@@ -0,0 +1,38 @@
+// classe Produto
+class Produto {
+
+    // atributos:
+    // nome;
+    // preco;
+    // estoque;
+
+    // método construtor
+    constructor(nome, preco, estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    // métodos
+    vender(quantidade) {
+        if (this.estoque >= quantidade) {
+            this.estoque -= quantidade;
+            console.log(`Venda realizada: ${quantidade} unidade(s) de ${this.nome}. Estoque atual: ${this.estoque}`);
+        } else {
+            console.log(`Estoque insuficiente de ${this.nome}! Estoque atual: ${this.estoque}`);
+        }
+    }
+
+    repor(quantidade) {
+        this.estoque += quantidade;
+        console.log(`Reposição efetuada: +${quantidade} unidade(s) de ${this.nome}. Estoque atual: ${this.estoque}`);
+    }
+
+    alterarPreco(novoPreco) {
+        this.preco = novoPreco;
+        console.log(`Preço do produto ${this.nome} alterado para: R$ ${this.preco.toFixed(2)}`);
+    }
+}
+
+// Exporta a classe para ser utilizada em outros arquivos
+module.exports = Produto;
